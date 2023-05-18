8ff5fc0993d4a5b81d6ddc148b050bffb1beaf52
NAKAJIMA Junichi <nacj4040.magicana@gmail.com>
2023-05-18 18:04:16 +0900

    updated

--- abc085c/abc085c.py
+++ abc085c/abc085c.py
@@ -1,9 +1,10 @@
 import time
 import os
+
 dirname = os.path.dirname(__file__)
 
-in_file = dirname + '/input1.txt'
-f = open(in_file, 'r', encoding='UTF-8')
+in_file = dirname + "/input1.txt"
+f = open(in_file, "r", encoding="UTF-8")
 
 start = time.time()
 
@@ -11,23 +12,18 @@ text = f.readlines()
 n, y = map(int, text[0].strip().split())
 # print(n, y)
 flag = 0
-answers = []
-for i in range(y//10000+1):
-    for j in range(y//5000+1):
-        for k in range(y//1000+1):
-            goukei = 10000*i + 5000*j + 1000*k
-            if goukei == y and i+j+k == n:
-                answers.append([i, j, k])
-                flag = 1
-        if flag==1:
-            break
-    if flag==1:
+for i in range(n + 1):
+    if flag == 1:
         break
+    for j in range(n - i + 1):
+        k = (y - 10000 * i - 5000 * j) // 1000
+        if k >= 0 and i + j + k == n:
+            print(i, j, k)
+            flag = 1
+            break
 
-if flag == 1:
-    print(" ".join(map(str, answers[0])))
 if flag == 0:
     print(-1, -1, -1)
 
-end = time.time()-start
-print(end,"秒")
+end = time.time() - start
+print(end, "秒")
