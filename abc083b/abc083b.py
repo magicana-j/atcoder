bdd61180044755951974b4590777323752883f1d
nakajimajunichi <nacj4040.magicana@gmail.com>
2023-05-19 19:36:47 +0900

    updated

--- abc083b/abc083b.py
+++ abc083b/abc083b.py
@@ -1,14 +1,21 @@
 import os
+
 dirname = os.path.dirname(__file__)
 
-in_file = dirname + '/input.txt'
-f = open(in_file, 'r', encoding='UTF-8')
+fname = "input.txt"
+in_file = dirname + "/" + fname
+f = open(in_file, "r", encoding="UTF-8")
+
+
+def solve(n: int, a: int, b: int):
+    goukei = 0
+    for i in range(1, n + 1):
+        my_list = list(map(int, str(i)))
+        sm = sum(my_list)
+        if sm >= a and sm <= b:
+            goukei += i
+    return goukei
+
 
 n, a, b = map(int, f.readline().strip().split())
-goukei = 0
-for i in range(1, n+1):
-    my_list = list(map(int, str(i)))
-    sm = sum(my_list)
-    if sm >= a and sm <= b:
-        goukei += i
-print(goukei)
+print(solve(n, a, b))
